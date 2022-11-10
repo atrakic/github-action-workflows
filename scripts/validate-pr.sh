d25d9e70282f7faaf1e52fbd24c99908d49335d4
Admir Trakic <atrakic@users.noreply.github.com>
2022-11-10 21:11:14 +0100

    Add pro-close

--- scripts/validate-pr.sh
+++ scripts/validate-pr.sh
@@ -7,12 +7,12 @@ main() {
     changed=$(git show --name-only --oneline HEAD)
 
     if [[ -z "$changed" ]]; then
+        echo "Sorry not files have been changed in this PR"
         exit 0
     fi
 
     local -i num_changed
     num_changed=$(echo "$changed" | wc -l |xargs)
-
     if ((num_changed > allowed)); then
         echo "Error: this PR has changes to multiple files then $allowed. Please create individual PRs less then $allowed." >&2
         exit 1
