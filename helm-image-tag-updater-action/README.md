6f0d563a33f2fb4532f0885d13c2f3894cd1eab6
atrakic <atrakic@users.noreply.github.com>
2024-03-11 11:33:46 +0100

    Add helm-image-tag-updater-action (#21)

---
+++ helm-image-tag-updater-action/README.md
@@ -0,0 +1,40 @@
+# Helm values image tag updater
+
+This action updates image tag value for a given helm package.
+
+## Inputs
+
+## `repo`
+
+**Required** The name of the github repository.
+
+## `gh_token`
+
+**Required** The Personal Access Token (PAT) that the action will use for git operations.
+
+## app_name
+
+**Required** The name of the helm package.
+
+## image_tag
+
+**Required** The image tag to be used in new helm package.
+
+## Outputs
+
+## `values`
+
+The contents of values.yaml file.
+
+## Example usage
+
+```
+uses: atrakic/github-actions/helm-image-tag-updater-action@main
+with:
+  repo: 'github.com/atrakic/helm-charts.git'
+  token: ${{ secrets.GIT_TOKEN }}
+  app_name: 'foo'
+  image_tag: 'v1.12'
+  commiter: '${{ github.actor }}'
+  author: '${{ github.actor }}@users.noreply.github.com'
+```
---
+++ helm-image-tag-updater-action/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+: "${GIT_REPO:?'You need to configure the GIT_REPO variable!'}"
+: "${GIT_PAT:?'You need to configure the GIT_PAT variable!'}"
+: "${APP_NAME:?'You need to configure the APP_NAME variable!'}"
+: "${IMAGE_TAG:?'You need to configure the IMAGE_TAG variable!'}"
+
+set -o errexit
+
+git clone https://"${GIT_PAT}"@"${GIT_REPO}" "${GIT_REPO}"
+
+# Change directory to the root of the repository
+cd "${GIT_REPO}" || exit 1
+
+# Find the app directory
+APP_DIRECTORY=$(find . -type d -name "$APP_NAME")
+
+# Update the image tag in the values file
+sed -i -e "s/imageTag: .*/imageTag: $IMAGE_TAG/g" "$APP_DIRECTORY/values.yaml"
+
+git config --global user.email "${GH_EMAIL:-no-reply@users.noreply.github.com}"
+git config --global user.name "${GH_NAME:-GitOps}"
+
+git add .
+git commit -m "Updated $APP_NAME to version: $IMAGE_TAG"
+git push
+
+# Workarround for github envs
+[[ -n "${GITHUB_ACTIONS}" ]] && git config --global --add safe.directory /github/workspace
+
+values=$(cat "$APP_DIRECTORY/values.yaml")
+echo "values=$values" >> "$GITHUB_OUTPUT"
